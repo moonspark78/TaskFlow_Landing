4e788db1c9871a6272afcce5f2b4facf706890f6
Omar Jabour <jabour.omar@hotmail.com>
2025-12-13 22:17:11 +0100

    :art: init the project adding images folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const font =  Rubik({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500","600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${font.className} antialiased`}
       >
+        
         {children}
+        
       </body>
     </html>
   );
