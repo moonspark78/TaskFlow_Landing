93acd96884580f89501e7626af0a09afad6f8c65
Omar Jabour <jabour.omar@hotmail.com>
2025-12-25 21:24:14 +0100

    :construction: Modify  Nav.tsx & Logo.tsx  :construction:

---
+++ components/Helper/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='flex items-center space-x-2'>Logo</div>
+  )
+}
+
+export default Logo
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,10 +1,12 @@
+import Logo from '@/components/Helper/Logo'
 import React from 'react'
 
 const Nav = () => {
   return (
     <div className='transition-all duration-200 h-[12vh] z-100 fixed w-full'>
       <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
-
+        {/* Logo */}
+        <Logo/>
       </div>
     </div>
   )
