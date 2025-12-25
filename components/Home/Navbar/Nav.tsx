5e43500e226a6ddba550fdf39ba7d3b8f90e7633
Omar Jabour <jabour.omar@hotmail.com>
2025-12-25 21:21:09 +0100

    :construction: Modify  Nav.tsx  :construction:

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='transition-all duration-200 h-[12vh] z-100 fixed w-full'></div>
+    <div className='transition-all duration-200 h-[12vh] z-100 fixed w-full'>
+      <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+
+      </div>
+    </div>
   )
 }
 
